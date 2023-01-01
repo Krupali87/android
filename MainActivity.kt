a573922bd362224cc3877601b8c6c0ede132d845
Krupali87 <119691073+Krupali87@users.noreply.github.com>
2023-01-01 16:44:51 +0530

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,32 +1,19 @@
-package com.example.implicit
+package com.example.explicit
 
 import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var t3 : TextView
-    lateinit var b4 : Button
+    lateinit var b1 : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        t3 = findViewById(R.id.t1)
-        b4 = findViewById(R.id.b1)
-        b4.setOnClickListener {
-            val url = "https://web.whatsapp.com/"
-            var i = Intent(Intent.ACTION_VIEW)
-            i.data = Uri.parse(url)
+        b1 = findViewById(R.id.btn1)
+        b1.setOnClickListener {
+            var i = Intent(applicationContext,MainActivity2::class.java)
             startActivity(i)
-
         }
     }
 }
-
-
-
-
-
--- MainActivity2.kt
+++ MainActivity2.kt
@@ -1,4 +1,4 @@
-package com.example.implicit
+package com.example.explicit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
