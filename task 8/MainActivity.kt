83a2b3bc8fc6f14a12a1badb9ce29124025d5ed2
Krupali87 <119691073+Krupali87@users.noreply.github.com>
2023-01-29 19:01:27 +0530

    Add files via upload

---
+++ task 8/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.task8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+
+class MainActivity : AppCompatActivity()
+{
+
+    lateinit var b1 :Button
+    lateinit var b2 : Button
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        b1 = findViewById(R.id.b1)
+        b2 = findViewById(R.id.b2)
+
+        b1.setOnClickListener {
+
+            var alerts = AlertDialog.Builder(this)
+            var layout : LayoutInflater = LayoutInflater.from(applicationContext)
+            var view = layoutInflater.inflate(R.layout.design,null)
+            alerts.setView(view)
+            alerts.show()
+
+        }
+        b2.setOnClickListener {
+
+            var alerts = AlertDialog.Builder(this)
+            var layout : LayoutInflater = LayoutInflater.from(applicationContext)
+            var view = layoutInflater.inflate(R.layout.design1,null)
+            alerts.setView(view)
+            alerts.show()
+        }
+    }
+}
